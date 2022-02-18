f4777003bb887eadddc729ba3a67cdf9851dc598
Emmanuel Adiba <eabaagah@gmail.com>
2022-02-18 14:42:36 +0000

    sorting streams

--- src/com/aweperi/streams/Movie.java
+++ src/com/aweperi/streams/Movie.java
@@ -1,12 +1,14 @@
 package com.aweperi.streams;
 
 public class Movie {
-    private String title;
-    private int likes;
+    private final String title;
+    private final int likes;
+    private final int rating;
 
-    public Movie(String title, int likes) {
+    public Movie(String title, int likes, int rating) {
         this.title = title;
         this.likes = likes;
+        this.rating = rating;
     }
 
     public String getTitle() {
@@ -17,11 +19,16 @@ public class Movie {
         return likes;
     }
 
+    public int getRating() {
+        return rating;
+    }
+
     @Override
     public String toString() {
         return "Movie{" +
                 "title='" + title + '\'' +
                 ", likes=" + likes +
+                ", rating=" + rating +
                 '}';
     }
 }
--- src/com/aweperi/streams/StreamsDemo.java
+++ src/com/aweperi/streams/StreamsDemo.java
@@ -1,5 +1,6 @@
 package com.aweperi.streams;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -7,32 +8,39 @@ import java.util.stream.Stream;
 public class StreamsDemo {
     public static void show() {
         var movies = List.of(
-                new Movie("Kate", 1000),
-                new Movie("Lucy", 4000),
-                new Movie("Panda", 1500)
+                new Movie("Kate", 1000, 4),
+                new Movie("Panda", 1500, 5),
+                new Movie("Lucy", 4000, 3),
+                new Movie("Iron Man", 1500, 4),
+                new Movie("Harry Potter", 1500, 5)
         );
 
-        // Map
-        movies.stream()
-                .map(Movie::getLikes)
-                .forEach(like -> System.out.println(like));
-
-        //Flatmap
-        Stream.of(List.of(1,2,3),List.of(4,5,6))
-                .flatMap(list -> list.stream())
-                .forEach(n -> System.out.println(n));
-
-        //Filter
-        Predicate<Movie> isPopular = movie -> movie.getLikes() > 1000;
-        movies.stream()
-                .filter(isPopular)
-                .forEach(m -> System.out.println(m));
+//        // Map
+//        movies.stream()
+//                .map(Movie::getLikes)
+//                .forEach(like -> System.out.println(like));
+//
+//        //Flatmap
+//        Stream.of(List.of(1,2,3),List.of(4,5,6))
+//                .flatMap(list -> list.stream())
+//                .forEach(n -> System.out.println(n));
+//
+//        //Filter
+//        Predicate<Movie> isPopular = movie -> movie.getLikes() > 1000;
+//        movies.stream()
+//                .filter(isPopular)
+//                .forEach(m -> System.out.println(m));
 
         // Slicing
-        movies.stream()
-                .skip(1)
-                .limit(1)
-                .forEach(m -> System.out.println(m));
+//        Predicate<Movie> isSuperAwesome = m -> m.getRating() > 4;
+//        movies.stream()
+//                .skip(1)
+//                .takeWhile(isSuperAwesome)
+//                .forEach(m -> System.out.println(m.getTitle()));
 
+        //Sorting
+        movies.stream()
+                .sorted(Comparator.comparing(Movie::getRating).reversed())
+                .forEach(System.out::println);
     }
 }
