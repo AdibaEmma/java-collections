12bfafe2ec04453ba7f82b20e4d31c861bc54e6f
Emmanuel Adiba <eabaagah@gmail.com>
2022-04-16 17:40:24 +0000

    create async object

---
+++ src/com/aweperi/executors/CompletableFuturesDemo.java
@@ -0,0 +1,20 @@
+package com.aweperi.executors;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
+
+public class CompletableFuturesDemo {
+    public static void show() {
+        Supplier<Integer> task = () -> 1;
+        var future = CompletableFuture.supplyAsync(task);
+        try {
+            System.out.println("Getting Results");
+            var result = future.get();
+            System.out.println("Result is: " + result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- src/com/aweperi/executors/Main.java
+++ src/com/aweperi/executors/Main.java
@@ -2,6 +2,6 @@ package com.aweperi.executors;
 
 public class Main {
     public static void main(String[] args) {
-        ExecutorsDemo.show();
+        CompletableFuturesDemo.show();
     }
 }
