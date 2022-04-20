82d5afb06301e0f3cfe2dcba6c3b757a4c8a8168
Emmanuel Adiba <eabaagah@gmail.com>
2022-04-20 08:50:13 +0000

    update

--- src/com/aweperi/executors/CompletableFuturesDemo.java
+++ src/com/aweperi/executors/CompletableFuturesDemo.java
@@ -16,5 +16,6 @@ public class CompletableFuturesDemo {
             e.printStackTrace();
         }
 
+        future.thenRun(() -> System.out.println("Done"));
     }
 }
--- src/com/aweperi/executors/Main.java
+++ src/com/aweperi/executors/Main.java
@@ -2,6 +2,16 @@ package com.aweperi.executors;
 
 public class Main {
     public static void main(String[] args) {
+//        var mailService = new MailService();
+//        mailService.sendAsync();
+//        System.out.println("Hello World");
+//
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
         CompletableFuturesDemo.show();
     }
 }
