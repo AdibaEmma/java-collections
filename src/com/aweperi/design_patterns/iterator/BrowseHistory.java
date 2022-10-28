6ca27d8736b6fcb8f562dfeb02ff4630013dc31d
Emmanuel Adiba <eabaagah@gmail.com>
2022-10-28 10:31:40 +0000

    update iterator pattern

--- src/com/aweperi/design_patterns/iterator/BrowseHistory.java
+++ src/com/aweperi/design_patterns/iterator/BrowseHistory.java
@@ -1,24 +1,52 @@
 package com.aweperi.design_patterns.iterator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrowseHistory {
-    private String[] urls = new String[3];
+//    private String[] urls = new String[3];
+    private List<String> urls = new ArrayList<String>();
     private int count;
 
-    void push(String url) { urls[count++] = url; }
+    void push(String url) { urls.add(url); count++; }
 
     public String pop() {
-        return urls[--count];
+        return urls.remove(--count);
     }
 
     public Iterator<String> createIterator() {
-        return new ArrayIterator(this);
+        return new ListIterator(this);
     }
 
-    public class ArrayIterator implements Iterator<String> {
+//    public class ArrayIterator implements Iterator<String> {
+//        private BrowseHistory history;
+//        private int index;
+//
+//        public ArrayIterator(BrowseHistory history) {
+//            this.history = history;
+//        }
+//
+//        @Override
+//        public boolean hasNext() {
+//            return (index < history.count);
+//        }
+//
+//        @Override
+//        public String current() {
+//            return history.urls[index];
+//        }
+//
+//        @Override
+//        public void next() {
+//            index++;
+//        }
+//    }
+
+    public class ListIterator implements Iterator<String> {
         private BrowseHistory history;
         private int index;
 
-        public ArrayIterator(BrowseHistory history) {
+        public ListIterator(BrowseHistory history) {
             this.history = history;
         }
 
@@ -29,7 +57,7 @@ public class BrowseHistory {
 
         @Override
         public String current() {
-            return history.urls[index];
+            return history.urls.get(index);
         }
 
         @Override
